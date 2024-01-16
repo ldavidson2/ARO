96fe37d8a7bdd8965f0673710049655fb02bb860
Leo Davidson <94934268+ldavidson2@users.noreply.github.com>
2024-01-15 22:57:29 -0600

    Fixed issue with too many threads Added a dialog history Minor Styling

--- aro/src/components/Home.js
+++ aro/src/components/Home.js
@@ -1,29 +1,57 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import "./Home.css";
 
 const Home = () => {
   const [message, setMessage] = useState();
   const [AROResponse, setAROResponse] = useState();
-  
+  const [dialogHistory, setDialogHistory] = useState([]);
+
   async function queryARO() {
+    addDialogToList(message);
     const response = await axios.get(`/queryARO/${message}`, {});
-    setAROResponse(response.data.data["0"].content["0"].text.value);
-    console.log(response.data);
+    const newDialog = response.data.data["0"].content["0"].text.value;
+    setAROResponse(newDialog);
+    addDialogToList(newDialog);
   }
 
   function onChange(event) {
     setMessage(event.target.value);
   }
 
+  const handleKeypress = (event) => {
+    if (event.key === "Enter") {
+      document.getElementById("buttons").click();
+    }
+  };
+
+  const addDialogToList = (newDialog) => {
+    setDialogHistory((prevList) => [...prevList, newDialog]);
+  };
+
   return (
     <div>
       <form>
         <p>{AROResponse}</p>
+        <div id="userInput">
         <input onChange={onChange}></input>
-        <button type="button" onClick={() => {
+        <button
+          type="button"
+          onClick={() => {
             queryARO();
-          }}>Submit</button>
+          }}
+        >
+          Submit
+        </button>
+        </div>
       </form>
+      <aside>
+        <ul>
+          {dialogHistory.map((str, index) => (
+            <li key={index}>{str}</li>
+          ))}
+        </ul>
+      </aside>
     </div>
   );
 };
